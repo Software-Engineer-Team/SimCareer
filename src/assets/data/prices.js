ae60fb322037740e3c30b8b246c5840d43bd1781
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2022-11-08 12:24:31 +0700

    [style]: Add animation for prices list

---
+++ src/assets/data/prices.js
@@ -0,0 +1,53 @@
+export const prices = [
+  {
+    url: "/images/free-price.jpg",
+    typeOfPrice: "MIỄN PHÍ",
+    typeOfPriceMore: "",
+    priceContents: [
+      "Cập nhật tin tức ngành nghề mà bạn quan tâm.",
+      "Cập nhật sự kiện trực tiếp và trực tuyến liên quan đến ngành nghề.",
+      "Cung cấp vị trí công việc liên quan đến ngành nghề đang có trên thị trường miễn phí.",
+      "Phân tích CV",
+    ],
+    btn: "Trải nghiệm ngay!",
+    btnColor: "#f25b64",
+    newGif: "/images/new-icon.gif",
+  },
+  {
+    url: "/images/plus-price.jpg",
+    typeOfPrice: "99.000đ/gói",
+    typeOfPriceMore: "SIMFREE và hơn thế nữa",
+    priceContents: [
+      "Chuyên gia đánh giá, nhận xét và gợi ý giải pháp sửa đổi CV.",
+      "Chuyên gia tư vấn thêm các nội dung: cách viết trong CV, phong cách thiết kế phù hợp với lĩnh vực bạn đang quan tâm…",
+      "Chuyên gia giải đáp các câu hỏi bạn đang băn khoăn về hồ sơ trong đơn đăng ký.",
+    ],
+    btn: "Đăng ký ngay!",
+    btnColor: "#dd366e",
+  },
+  {
+    url: "/images/pro-price.jpg",
+    typeOfPrice: "199.000đ/gói",
+    typeOfPriceMore: "SIMPLUS và hơn thế nữa",
+    priceContents: [
+      "Chuyên gia nhận xét và gợi ý cách sửa thiết kế, câu từ tiếng Anh, chỉ lỗi ngữ pháp để ghi điểm với nhà tuyển dụng, tăng cơ hội mời phỏng vấn.",
+      "Dựa trên bản CV Tiếng Việt từ bạn, chuyên gia sẽ dịch sang Tiếng Anh đúng chuẩn và chuyên nghiệp.",
+      "Các chuyên gia từ SIMCareer sẽ sử dụng các từ khóa tốt nhất, mạnh nhất, phù hợp nhất với vị trí và lĩnh vực, tăng khả năng trúng tuyển của bạn.",
+    ],
+    btn: "Đăng ký ngay!",
+    btnColor: "#00a5dd",
+  },
+  {
+    url: "/images/max-price.jpg",
+    typeOfPrice: "599.000đ/gói",
+    typeOfPriceMore: "SIMPRO và hơn thế nữa",
+    priceContents: [
+      "Kho video phỏng vấn mẫu vị trí tương tự trong lĩnh vực mà bạn quan tâm.",
+      "Hướng dẫn chuyên sâu kỹ năng phỏng vấn (tư vấn 1-1).",
+      "Virtual Intern",
+    ],
+    btn: "Đăng ký ngay!",
+    btnColor: "#894eb3",
+    newGif: "/images/comming-soon-icon.gif",
+  },
+];
