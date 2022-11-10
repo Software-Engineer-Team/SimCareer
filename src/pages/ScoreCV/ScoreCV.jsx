23ac4477c21316015b9823fca5de8320414bb233
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2022-11-10 11:23:41 +0700

    [fix]: fix conflict

--- src/pages/ScoreCV/ScoreCV.jsx
+++ src/pages/ScoreCV/ScoreCV.jsx
@@ -16,6 +16,7 @@ import {
   RadialLinearScale,
 } from "chart.js";
 import { PieChart, RadarChart, WaveAnimation } from "@components/index";
+import { Link } from "react-router-dom";
 
 ChartJS.register(
   RadialLinearScale,
@@ -27,134 +28,6 @@ ChartJS.register(
 );
 
 const ScoreCV = () => {
-  const delayed = useRef(null);
-
-  const data = {
-    labels: [
-      "Trình độ học vấn",
-      "Kĩ năng",
-      "Hình thức",
-      "Thương hiệu cá nhân",
-      "Kinh nghiệm",
-    ],
-    datasets: [
-      {
-        label: "Điểm mạnh",
-        data: [58.3, 50, 58.3, 40, 41.7],
-        fill: true,
-        backgroundColor: "#841a9e95",
-        borderColor: "#841a9e95",
-        pointBackgroundColor: "#841a9e95",
-        pointBorderColor: "#841a9e95",
-        pointHoverBackgroundColor: "#841a9e",
-        pointHoverBorderColor: "#841a9e",
-      },
-    ],
-  };
-
-  const options = {
-    elements: {
-      line: {
-        borderWidth: 3,
-      },
-      point: {
-        borderWidth: 1,
-      },
-    },
-    animation: {
-      onComplete: () => {
-        delayed.current = true;
-      },
-      delay: (context) => {
-        let delay = 0;
-        if (
-          context.type === "data" &&
-          context.mode === "default" &&
-          !delayed.current
-        ) {
-          delay = context.dataIndex * 300 + context.datasetIndex * 100;
-        }
-        return delay;
-      },
-    },
-    hoverRadius: 12,
-    interaction: {
-      mode: "nearest",
-      intersect: false,
-      axis: "x",
-    },
-    responsive: true,
-    plugins: {
-      title: {
-        display: true,
-        text: "Đánh giá chung",
-        padding: {
-          top: 0,
-        },
-        font: {
-          size: 35,
-        },
-        color: "#000000",
-      },
-      tooltip: {
-        enabled: true,
-        position: "average",
-        titleFont: {
-          size: 20,
-          color: "#ffffff",
-          lineHeight: 1.2,
-        },
-        bodyFont: {
-          size: 18,
-          color: "#ffffff",
-          lineHeight: 1.2,
-        },
-        bodyAlign: "left",
-        padding: 10,
-        boxPadding: 10,
-      },
-      legend: {
-        display: true,
-        position: "chartArea",
-        align: "start",
-        labels: {
-          padding: 150,
-          font: {
-            size: 25,
-          },
-          color: "#000000",
-        },
-      },
-    },
-    maintainAspectRatio: true,
-    scales: {
-      r: {
-        ticks: {
-          display: false,
-          color: "#000000",
-          font: {
-            size: 25,
-          },
-          stepSize: 16.7,
-        },
-        angleLines: {
-          color: "#000000",
-        },
-        min: 0,
-        max: 100,
-        grid: {
-          color: "#000000",
-        },
-        pointLabels: {
-          color: "#595857",
-          font: {
-            size: 25,
-          },
-        },
-      },
-    },
-  };
-
   return (
     <ScoreCVContainer>
       <ScoreCVContent>
@@ -187,7 +60,18 @@ const ScoreCV = () => {
               </div>
               <PieChart />
             </div>
-            {/* <div className="specific-chart"></div> */}
+          </div>
+
+          <div className="join-with-us">
+            <h3>Tham gia ngay cùng SIMCareer để gia tăng cơ hội tuyển dụng</h3>
+            <div className="join-logo">
+              <img src="/images/simcareer-not-label.png" alt="" />
+            </div>
+            <div className="join-btn">
+              <Link to={"#"} className="join-btn-content">
+                <span>Tiếp tục</span>
+              </Link>
+            </div>
           </div>
           <WaveAnimation top={100} height={"900px"} />
           <WaveAnimation top={1300} height={"100vh"} />
--- src/pages/ScoreCV/ScoreCV.styled.jsx
+++ src/pages/ScoreCV/ScoreCV.styled.jsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const ScoreCVContainer = styled.div`
   width: 100%;
-  height: 100%;
   background-color: #2d3d6e;
+  max-height: 100%;
+  overflow-y: auto;
 `;
 
 export const ScoreCVContent = styled.div`
@@ -44,11 +45,11 @@ export const ScoreCVBody = styled.div`
   padding: 15px 0;
   height: 100%;
   width: 100%;
-  overflow-y: auto;
 
   .score-show-container {
     max-width: 1400px;
     margin: 0 auto;
+    /* min-height: 300px; */
 
     .score-show-content {
       display: flex;
@@ -56,9 +57,35 @@ export const ScoreCVBody = styled.div`
 
       h3 {
         margin: 0;
-        font-size: 72px;
-        color: #fff;
-        background: -webkit-linear-gradient(#eee, #333);
+        font-size: 42px;
+        text-align: center;
+        text-transform: uppercase;
+        background: rgb(255, 255, 255);
+        background: linear-gradient(
+          90deg,
+          rgba(255, 255, 255, 1) 0%,
+          rgba(255, 255, 255, 1) 0%,
+          rgba(209, 208, 209, 1) 100%
+        );
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+
+      .score-number {
+        font-size: 152px;
+        font-family: "Anton", sans-serif;
+        letter-spacing: 1.2px;
+        text-align: center;
+
+        background: rgb(211, 25, 213);
+        background: linear-gradient(
+          90deg,
+          rgba(211, 25, 213, 1) 0%,
+          rgba(138, 90, 199, 1) 0%,
+          rgba(180, 117, 189, 1) 100%
+        );
+        background-size: 200% auto;
         background-clip: text;
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
@@ -67,6 +94,72 @@ export const ScoreCVBody = styled.div`
           to {
             background-position: 200% center;
           }
+        }
+      }
+    }
+  }
+
+  .join-with-us {
+    max-width: 1400px;
+    margin: 0 auto;
+    margin-bottom: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    --btn-color: #771fd3;
+
+    h3 {
+      font-size: 30px;
+      font-style: italic;
+      font-family: Cambria, Georgia, serif;
+      font-weight: 700;
+    }
+
+    .join-logo {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      margin-bottom: 20px;
+
+      img {
+        width: 90px;
+        height: 90px;
+        object-fit: cover;
+      }
+    }
+
+    .join-btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 10px;
+
+      .join-btn-content {
+        padding: 5px 0px;
+        width: 150px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: var(--btn-color);
+        border-radius: 10px;
+        color: #ffffff;
+        transition: all 100ms ease-in;
+        border: 1px solid var(--btn-color);
+        cursor: pointer;
+        text-decoration: none;
+
+        &:hover {
+          background-color: #ffffff;
+          color: var(--btn-color);
+        }
+
+        & > span {
+          font-family: "Anton", sans-serif;
+          font-size: 17px;
+          text-transform: uppercase;
+          font-weight: 400 !important;
         }
       }
     }
